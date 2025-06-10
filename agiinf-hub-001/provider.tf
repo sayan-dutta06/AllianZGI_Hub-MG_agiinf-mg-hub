d4700116a3f32110e307d179730b927e8dedb6c6
Shubham Pandey <shubham.pandey25@infosys.com>
2025-06-10 10:30:52 +0530

    rg code is created

---
+++ agiinf-hub-001/provider.tf
@@ -0,0 +1,12 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "4.32.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  # Configuration options
+}
